003be3ce2556ec61c4960b23d94bcc051df2e975
Алексей <77805051+Morgeous29@users.noreply.github.com>
2024-11-11 03:24:11 +0300

    Update module_3_2.py

--- module_3_2.py
+++ module_3_2.py
@@ -1,13 +1,13 @@
 def send_email(message, recipient,  sender = "university.help@gmail.com"):
     if '@' not in recipient or '@' not in sender or not recipient.endswith(
             ('.com', '.ru', '.net')) or not sender.endswith(('.com', '.ru', '.net')):
-        print(f'Невозможно отправить письмо с адреса {sender} на адрес {recipient}')
+        print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient + '.')
     elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
     elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса {sender} на адрес {recipient}.')
+        print('Письмо успешно отправлено с адреса', sender, 'на адрес', recipient + '.')
     elif sender != 'university.help@gmail.com':
-        print(f'НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.')
+        print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса', sender, 'на адрес', recipient + '.')
 
 send_email('Это письмо невозможно отправить по первому условию программы', 'lexakirillov290497@mail.ru', 'lexakirillov290497@mail.uk')
 send_email('Это письмо невозможно отправить по второму условию программы', 'university.help@gmail.com', 'university.help@gmail.com')
